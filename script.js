23fbb037e2e82d25be5dbd23282508a8b0d2520b
rorylkd <rory.donaldson14@gmail.com>
2021-04-05 21:00:25 +0800

    Added functions to save text in each individual field

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ else if(currentHour === "5PM"){
     fourEl.style.backgroundColor = "#d3d3d3";
     fiveEl.style.backgroundColor = "#ff6961";
 }
-else if(currentHour === ("6PM" || "7PM" || "8PM" || "9PM" || "10PM" || "11PM")){ //Edgecase #1: hours later than 5pm
+else if(currentHour === "6PM"){ 
     nineEl.style.backgroundColor = "#d3d3d3";
     tenEl.style.backgroundColor = "#d3d3d3";
     elevenEl.style.backgroundColor = "#d3d3d3";
@@ -132,7 +132,62 @@ else if(currentHour === ("6PM" || "7PM" || "8PM" || "9PM" || "10PM" || "11PM")){
     fourEl.style.backgroundColor = "#d3d3d3";
     fiveEl.style.backgroundColor = "#d3d3d3";
 }
-else if(currentHour === ("12AM" || "1AM" || "2AM" || "3AM" || "4AM" || "5AM" || "6AM" || "7AM" || "8AM")){ //Edgecase #2: hours earlier than 9am. I know I could've just used else, but I like to write things out apparently.
+else if(currentHour === "7PM"){ 
+    nineEl.style.backgroundColor = "#d3d3d3";
+    tenEl.style.backgroundColor = "#d3d3d3";
+    elevenEl.style.backgroundColor = "#d3d3d3";
+    noonEl.style.backgroundColor = "#d3d3d3";
+    oneEl.style.backgroundColor = "#d3d3d3";
+    twoEl.style.backgroundColor = "#d3d3d3";
+    threeEl.style.backgroundColor = "#d3d3d3";
+    fourEl.style.backgroundColor = "#d3d3d3";
+    fiveEl.style.backgroundColor = "#d3d3d3";
+}
+else if(currentHour === "8PM"){ 
+    nineEl.style.backgroundColor = "#d3d3d3";
+    tenEl.style.backgroundColor = "#d3d3d3";
+    elevenEl.style.backgroundColor = "#d3d3d3";
+    noonEl.style.backgroundColor = "#d3d3d3";
+    oneEl.style.backgroundColor = "#d3d3d3";
+    twoEl.style.backgroundColor = "#d3d3d3";
+    threeEl.style.backgroundColor = "#d3d3d3";
+    fourEl.style.backgroundColor = "#d3d3d3";
+    fiveEl.style.backgroundColor = "#d3d3d3";
+}
+else if(currentHour === "9PM"){ 
+    nineEl.style.backgroundColor = "#d3d3d3";
+    tenEl.style.backgroundColor = "#d3d3d3";
+    elevenEl.style.backgroundColor = "#d3d3d3";
+    noonEl.style.backgroundColor = "#d3d3d3";
+    oneEl.style.backgroundColor = "#d3d3d3";
+    twoEl.style.backgroundColor = "#d3d3d3";
+    threeEl.style.backgroundColor = "#d3d3d3";
+    fourEl.style.backgroundColor = "#d3d3d3";
+    fiveEl.style.backgroundColor = "#d3d3d3";
+}
+else if(currentHour === "10PM"){ 
+    nineEl.style.backgroundColor = "#d3d3d3";
+    tenEl.style.backgroundColor = "#d3d3d3";
+    elevenEl.style.backgroundColor = "#d3d3d3";
+    noonEl.style.backgroundColor = "#d3d3d3";
+    oneEl.style.backgroundColor = "#d3d3d3";
+    twoEl.style.backgroundColor = "#d3d3d3";
+    threeEl.style.backgroundColor = "#d3d3d3";
+    fourEl.style.backgroundColor = "#d3d3d3";
+    fiveEl.style.backgroundColor = "#d3d3d3";
+}
+else if(currentHour === "11PM"){ 
+    nineEl.style.backgroundColor = "#d3d3d3";
+    tenEl.style.backgroundColor = "#d3d3d3";
+    elevenEl.style.backgroundColor = "#d3d3d3";
+    noonEl.style.backgroundColor = "#d3d3d3";
+    oneEl.style.backgroundColor = "#d3d3d3";
+    twoEl.style.backgroundColor = "#d3d3d3";
+    threeEl.style.backgroundColor = "#d3d3d3";
+    fourEl.style.backgroundColor = "#d3d3d3";
+    fiveEl.style.backgroundColor = "#d3d3d3";
+}
+else { //Edgecase #2: hours earlier than 9am
     nineEl.style.backgroundColor = "#ff6961";
     tenEl.style.backgroundColor = "#ff6961";
     elevenEl.style.backgroundColor = "#ff6961";
@@ -144,18 +199,86 @@ else if(currentHour === ("12AM" || "1AM" || "2AM" || "3AM" || "4AM" || "5AM" || 
     fiveEl.style.backgroundColor = "#ff6961";
 }
 
-function saveText() {
-    console.log("Write this");
+
+
+
+function saveText9am() { // Function to save text in the textarea element.
+    var myText9am = document.getElementById("9am").value;
+    localStorage.setItem("myText9am", myText9am);
+    console.log("Does this still work?");
 
 }
 
-document.getElementById("saveBtn1").addEventListener("click", saveText);
-document.getElementById("saveBtn2").addEventListener("click", saveText);
-document.getElementById("saveBtn3").addEventListener("click", saveText);
-document.getElementById("saveBtn4").addEventListener("click", saveText);
-document.getElementById("saveBtn5").addEventListener("click", saveText);
-document.getElementById("saveBtn6").addEventListener("click", saveText);
-document.getElementById("saveBtn7").addEventListener("click", saveText);
-document.getElementById("saveBtn8").addEventListener("click", saveText);
-document.getElementById("saveBtn9").addEventListener("click", saveText);
+function saveText10am() { 
+    var myText10am = document.getElementById("10am").value;
+    localStorage.setItem("myText10am", myText10am);
+    console.log("Does this still work?");
+
+}
+
+function saveText11am() { 
+    var myText11am = document.getElementById("11am").value;
+    localStorage.setItem("myText11am", myText11am);
+    console.log("Does this still work?");
+
+}
+
+function saveText12pm() { 
+    var myText12pm = document.getElementById("12pm").value;
+    localStorage.setItem("myText12pm", myText12pm);
+    console.log("Does this still work?");
+
+}
+
+function saveText1pm() { 
+    var myText1pm = document.getElementById("1pm").value;
+    localStorage.setItem("myText1pm", myText1pm);
+    console.log("Does this still work?");
+
+}
+
+function saveText2pm() { 
+    var myText2pm = document.getElementById("2pm").value;
+    localStorage.setItem("myText2pm", myText2pm);
+    console.log("Does this still work?");
+
+}
+
+function saveText3pm() { 
+    var myText3pm = document.getElementById("3pm").value;
+    localStorage.setItem("myText3pm", myText3pm);
+    console.log("Does this still work?");
+
+}
+
+function saveText4pm() { 
+    var myText4pm = document.getElementById("4pm").value;
+    localStorage.setItem("myText4pm", myText4pm);
+    console.log("Does this still work?");
+
+}
+
+function saveText5pm() { 
+    var myText5pm = document.getElementById("5pm").value;
+    localStorage.setItem("myText5pm", myText5pm);
+    console.log("Does this still work?");
+
+}
+
+function loadText9am() {
+    var myText9am = localStorage.getItem("myText9am");
+    document.getElementById("9am").value = myText9am;
+}
+
+document.getElementById("saveBtn1").addEventListener("click", saveText9am); // Event listeners for each individual button.
+document.getElementById("saveBtn2").addEventListener("click", saveText10am);
+document.getElementById("saveBtn3").addEventListener("click", saveText11am);
+document.getElementById("saveBtn4").addEventListener("click", saveText12pm);
+document.getElementById("saveBtn5").addEventListener("click", saveText1pm);
+document.getElementById("saveBtn6").addEventListener("click", saveText2pm);
+document.getElementById("saveBtn7").addEventListener("click", saveText3pm);
+document.getElementById("saveBtn8").addEventListener("click", saveText4pm);
+document.getElementById("saveBtn9").addEventListener("click", saveText5pm);
+
+
 
